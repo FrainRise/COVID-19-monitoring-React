97fc02af8bc430ecf62280b6f35f1a947d597586
FrainRise <frainrise@gmail.com>
2020-05-30 12:55:33 +0300

    Edited Navbar. Added sticky logic for Navbar and hover effects for list elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,56 @@
 import React from 'react'
-import {ReactNavbar} from 'react-responsive-animate-navbar'
+import { FaAlignLeft } from 'react-icons/fa'
+
 import virusIcon from '../../images/virus.png'
 
 import  './Navbar.css'
 
-class Navbar extends React.Component {
+export default class Navbar extends React.Component {
+    state = {
+        toggle: false
+    }
+
+    toggleBtn = () =>{
+        this.setState({ toggle: !this.state.toggle })
+    }
+
+    componentDidMount() {
+        const header = document.getElementById('customHeader');
+        const sticky = header.offsetTop;
+        const scrollCallback = window.addEventListener('scroll', () =>{
+            if(window.pageYOffset > sticky){
+                header.classList.add('sticky');
+            } else {
+                header.classList.remove('sticky');
+            }
+        });
+        return () => {
+            window.removeEventListener('scroll', scrollCallback)
+        }
+    }
 
     render(){
         return (
-            <div className='container'>
-                <ReactNavbar 
-                    color='rgba(255, 0, 0, 0.5)'
-                    logo={virusIcon}
-                />
+            <div className='navBar' id="customHeader">
+                <button onClick={this.toggleBtn}>
+                    <FaAlignLeft />
+                </button>
+                <ul className={this.state.toggle ? 
+                    'nav-links show-nav'
+                    : 'nav-links'
+                    }
+                >
+                    <li> 
+                        <a href="/#" className="link link--kukuri" data-letters="Home">Home</a>
+                    </li>
+                    <li> 
+                        <a href="/#" className="link link--kukuri" data-letters="About Project">About Project</a> 
+                    </li>
+                    <li> 
+                        <a href="/#" className="link link--kukuri" data-letters="Contact Us">Contact Us</a>
+                    </li>
+                </ul>
             </div>
         )
     }
 }
-
-export default Navbar;
