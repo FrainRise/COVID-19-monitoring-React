eb5147b24c86f899a25eebad65f7136e2e13d76c
FrainRise <frainrise@gmail.com>
2020-06-03 10:54:56 +0300

    Added some information for introduction section. Added new fonts.

--- src/components/MainPage/Introduction/Introduction.jsx
+++ src/components/MainPage/Introduction/Introduction.jsx
@@ -6,7 +6,19 @@ import styles from './Introduction.module.css'
 const Introduction = () => {
     return(
         <div className={styles.wrapper}>
-           <h2>COVID-19 - potentially severe acute coronavirus respiratory infection</h2>
+            <div className={styles.container}>
+                <div className={styles.row}>
+                    <h2 className={styles.titleCovid}>COVID-19 - potentially severe acute coronavirus respiratory infection.</h2>
+                    <p className={styles.descriptionCovid}>
+                    It is a dangerous disease, which can occur both in the form of an acute respiratory mild viral infection 
+                    and in a severe form, specific complications of which may include viral pneumonia, which entails acute 
+                    respiratory distress Syndrome or respiratory failure at risk of death.
+                    </p>
+                </div>
+                <div className={styles.row}>
+                    <h2 className={styles.hashtag}>#Staysafestayhome</h2>
+                </div>
+            </div>
         </div>
     )
 }
