32c0595cd0f78c2d7941a77072499d612fa796bc
FrainRise <frainrise@gmail.com>
2020-06-19 12:57:52 +0300

    Added assets for future components. Added empty templates: RulesSection and SymptomsSection.

---
+++ src/components/MainPage/RulesSection/RulesSection.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './RulesSection.css'
+
+export  const RulesSection = () => {
+    return (
+        <div className="rules-container">
+            <h2>It's RulesSection Component</h2>
+        </div>
+    )
+}
---
+++ src/components/MainPage/SymptomsSection/SymptomsSection.js
@@ -0,0 +1,11 @@
+import React from 'React'
+
+import './SymptomsSection.css'
+
+export const SymptomsSection = () => {
+    return (
+        <div className="symptoms-container">
+            <h2> It's SymptomsSection component</h2>
+        </div>
+    )
+}
