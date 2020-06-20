b02f3780688862d23a7ef405ffe600cd4c2a5fe4
FrainRise <frainrise@gmail.com>
2020-06-20 13:01:45 +0300

    Some changes with styles. Added background to make active class for li items

--- src/components/MainPage/Loader/Loader.js
+++ src/components/MainPage/Loader/Loader.js
@@ -4,7 +4,7 @@ import './Loader.css'
 
 const Loader = () => {
     return (
-        <div class="lds-ellipsis">
+        <div className="lds-ellipsis">
             <div className="ellipsis-item"></div>
             <div className="ellipsis-item"></div>
             <div className="ellipsis-item"></div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,16 +7,24 @@ import './responsive-navbar.css'
 
 export default class Navbar extends React.Component {
     state = {
-        toggle: false
+        isToggled: false,
+        isActive: false,
+        selectedLinkId: 1
     }
 
-    toggleBtn = () =>{
-        this.setState({ toggle: !this.state.toggle })
+    toggleBtn = () => {
+        this.setState({ isToggled: !this.state.isToggled })
+    }
+
+    addClassActive = () => {
+        this.setState({ isActive: !this.state.isActive })
+        console.log('acitve is', this.state.isActive )
     }
 
     componentDidMount() {
         const header = document.getElementById('customHeader');
         const sticky = header.offsetTop;
+        // Locate our scrolling to make Navbar
         const scrollCallback = window.addEventListener('scroll', () =>{
             if(window.pageYOffset > sticky){
                 header.classList.add('sticky');
@@ -24,42 +32,58 @@ export default class Navbar extends React.Component {
                 header.classList.remove('sticky');
             }
         });
+        
         return () => {
             window.removeEventListener('scroll', scrollCallback)
         }
     }
 
     render(){
+        const navLinks = [
+            {id: 1, name: 'Home', route: '/'},
+            {id: 2, name: 'About Project', route: '/about-project'},
+            {id: 3, name: 'Contact Us', route: '/contact-us'}
+        ]
         return (
             <div className='navBar' id="customHeader">
-                {/*<button onClick={this.toggleBtn} className='navBtn'>
-                    <FaAlignLeft />
-                </button> */}
-                <div className={this.state.toggle ? 'button-container clicked' : 'button-container '} onClick={this.toggleBtn}>
+                <div className={this.state.isToggled ? 'button-container clicked' : 'button-container '} onClick={this.toggleBtn}>
                     <div className="first-bar"></div>
                     <div className="second-bar"></div>
                     <div className="third-bar"></div>
                 </div>
-                <ul className={this.state.toggle ? 
+                <ul className={this.state.isToggled ? 
                     'nav-links show-nav'
                     : 'nav-links hide-nav-link'
                     }
                 >
-                    <Link to='/'>
+                    { navLinks.map((link) => {
+                      return (
+                        <Link 
+                            key={link.id} 
+                            to={link.route}
+                        >
+                            <li onClick={this.addClassActive}> 
+                                <h2 className="link link--kukuri" data-letters={link.name}>{link.name}</h2>
+                            </li>
+                        </Link>
+                      )  
+                    })
+                    }
+                    {/* <Link to='/'>
                         <li> 
                             <h2 className="link link--kukuri" data-letters="Home">Home</h2>
                         </li>
                     </Link>
-                    <Link to='/about-project'>
+                   <Link to='/about-project'>
                         <li> 
-                            <h2 className="link link--kukuri" data-letters="About Project">About Project</h2> 
+                            <h2 className={this.state.isActive ? "link link--kukuri active" : "link link--kukuri"} data-letters="About Project">About Project</h2> 
                         </li>
                     </Link>
                     <Link to='/contact-us'>
                         <li> 
                             <h2  className="link link--kukuri" data-letters="Contact Us">Contact Us</h2>
                         </li>
-                    </Link>                    
+                    </Link>    */}                
                 </ul>
             </div>
         )
