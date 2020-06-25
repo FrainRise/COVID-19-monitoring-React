0847fb3f6c046f702e33d2b852ce04d3beb3feb2
FrainRise <frainrise@gmail.com>
2020-06-25 13:07:27 +0300

    Edited and made full responsive AddressSection component

--- src/components/ContactPage/AddressSection/AddressSection.js
+++ src/components/ContactPage/AddressSection/AddressSection.js
@@ -1,24 +1,37 @@
 import React from 'react'
 
 import map from '../../../assets/images/address-icons/map.png'
+import address from '../../../assets/images/address-icons/address.png'
+import mail from '../../../assets/images/address-icons/mail.png'
+import phone from '../../../assets/images/address-icons/phone.png'
 
 import './AddressSection.css'
 
 const addresCreds = [
-    {id: 1, name: '', image: '', alt: ''},
-    {id: 1, name: '', image: '', alt: ''},
-    {id: 1, name: '', image: '', alt: ''}
+    {id: 1, name: 'Address', image: address, alt: 'Address', description: 'Erevanskiy 4/35, Slavutych, UA'},
+    {id: 2, name: 'Mail', image: mail, alt: 'Mail', description: 'risen-risen@gmail.com, cov-10-mon@gmail.com'},
+    {id: 3, name: 'Phone', image: phone, alt: 'Phone', description: '+380678765656, +790234456776'}
 ]
 
 const AddressSection = () => {
     return (
-        <div className="map-container">
+        <div className="address-container">
             <h2 className="local-title"> Our location </h2>
-            <div className="local-address">
-                                
-            </div>
-            <div className="map-block">
-            <img src={map} alt="map" className="map-image"/>
+            <div className="location-wrapper">
+                <div className="local-address">
+                    {addresCreds.map((address) => {
+                        return (
+                            <div className="address-wrapper">
+                                <img src={address.image} alt={address.alt} className="address-icon"/>
+                                <h2 className="address-title">{address.name}</h2>
+                                <p className="address-descr">{address.description}</p>
+                            </div>
+                        )
+                    })}           
+                </div>
+                <div className="map-block">
+                    <img src={map} alt="map" className="map-image"/>
+                </div>
             </div>
         </div>
     )
